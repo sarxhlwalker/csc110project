9f17e08a825ef8dbf2ac05dd23303683d2c11688
sarxhlwalker <sarxhlwalker@gmail.com>
2021-12-01 00:39:31 -0500

    update

--- test.py
+++ test.py
@@ -5,20 +5,27 @@ def test(filename: str, lst: list[str]):
     Save only the desired columns in lst from filename as a DataFrame.
 
     Note: filename should have double backslashes in the path.
+
+    Generalization untested.
     """
     file = pd.read_csv(filename, usecols=lst)
     return file
 
 
-def sort_file(city_migration):
+def sort_file(dataframe, keywords: set[str], column: str):
     """
-    WIP: generalizing sorting by row.
+    Create and return a new DataFrame containing only rows whose specified column are a
+        specific keyword.
+
+    Ex. city_migration; keywords = {'Net interprovincial migration',
+            'Net intraprovincial migration'}, columnn = 'Components of population growth'.
+
+    Generalization untested.
     """
     lst = []
-    keywords = {'Net interprovincial migration', 'Net intraprovincial migration'}
-    for x in range(len(city_migration)):
-        if city_migration.loc[x, 'Components of population growth'] in keywords:
-            lst.append(city_migration.loc[x])
+    for x in range(len(dataframe)):
+        if dataframe.loc[x, column] in keywords:
+            lst.append(dataframe.loc[x])
     return pd.DataFrame(lst)
 
 
