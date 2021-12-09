f7d3c56fd1d83ae5727fdf18605e0fcf181acd68
sarxhlwalker <sarxhlwalker@gmail.com>
2021-12-08 19:34:25 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,7 @@ def test(filename: str, lst: list[str]):
     """
     Save only the desired columns in lst from filename as a DataFrame.
 
-    Note: filename should have double backslashes in the path.
-
-    Ex. file = test('Data Sets/city migration and others.csv', ['REF_DATE', 'GEO', \
+    >>> file = test('Data Sets/city migration and others.csv', ['REF_DATE', 'GEO', \
                 'Components of population growth', 'VALUE'])
     """
     file = pd.read_csv(filename, usecols=lst)
@@ -18,10 +16,11 @@ def sort_file(dataframe, keywords: set[str], column: str):
     Create and return a new DataFrame containing only rows whose specified column are a
         specific keyword.
 
-    Ex. city_migration; keywords = {'Net interprovincial migration',
-            'Net intraprovincial migration'}, column = 'Components of population growth'.
-
     Generalization untested.
+    >>> file = test('Data Sets/city migration and others.csv', ['REF_DATE', 'GEO', \
+                'Components of population growth', 'VALUE'])
+    >>> sorted_file = sort_file(file,{'Net interprovincial migration', \
+            'Net intraprovincial migration'}, 'Components of population growth')
     """
     lst = []
     for x in range(len(dataframe)):
@@ -35,17 +34,15 @@ def split_file(dataframe):
     Split city_migration data into two separate DataFrames; one for intraprovincial
         migration and the other for interprovincial.
 
-    file = test('Data Sets/city migration and others.csv', ['REF_DATE', 'GEO', \
+    >>> file = test('Data Sets/city migration and others.csv', ['REF_DATE', 'GEO', \
                 'Components of population growth', 'VALUE'])
-    sorted_file = sort_file(file, {'Net interprovincial migration', \
+    >>> sorted_file = sort_file(file, {'Net interprovincial migration', \
             'Net intraprovincial migration'}, 'Components of population growth')
-    split_file(sorted_file)
+    >>> split = split_file(sorted_file)
     """
     inter = dataframe[dataframe['Components of population growth'] == 'Net interprovincial migration']
     intra = dataframe[dataframe['Components of population growth'] == 'Net intraprovincial migration']
     return (inter, intra)
-
-
 
 
 if __name__ == '__main__':
